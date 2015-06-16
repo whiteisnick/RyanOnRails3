8a41c29d6c38699d1c9095976a725a53d79d4fcc
whiteisnick <whiteisnick@gmail.com>
2015-06-16 18:09:02 -0400

    added show view content. Sort of got working next and previous links for entries...

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,3 +1,11 @@
 class Entry < ActiveRecord::Base
   belongs_to :collection
+
+  def previous
+    Entry.where('entries.id < ?', self.id).first
+  end
+
+  def next
+    Entry.where('entries.id > ?', self.id).last
+  end
 end
